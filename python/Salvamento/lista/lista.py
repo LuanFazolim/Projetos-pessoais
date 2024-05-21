e136defd7bdd24fc1c5681370a9e1fdded8cdb06
LuanFazolim <131609670+LuanFazolim@users.noreply.github.com>
2024-05-21 20:46:28 -0300

    Add files via upload

--- python/Salvamento/lista/lista.py
+++ python/Salvamento/lista/lista.py
@@ -1,6 +1,6 @@
 import os
 # Diretório que você deseja definir como o novo diretório de trabalho
-novo_diretorio = "E:\Python luan\python\Salvamento\lista"
+novo_diretorio = "C:\python\salvamento\listas"
 
 # Alterar o diretório de trabalho
 os.chdir(novo_diretorio)
@@ -14,28 +14,59 @@ print("Diretório atual:", os.getcwd())
 print("Arquivos no diretório:", os.listdir())
             
 
-
+sn = "s"
 total = list()
 corda = list()
 cont = 0
+con = 0
+err = 1
 
-
-
-with open("usu.txt","a") as esc:
-    nome = input("nome: ")
-    senha = input("senha: ")
-    abr = esc.write(f"\n{nome}\n\n{senha}\n")
-
+#add dados a lista 
 with open("usu.txt","r",encoding="utf-8") as usu:
     for o in usu:
         ler = usu.readline().strip()
         corda.append(ler)
-        print(ler)
         cont+=1
         if cont >=2:
             cont = 0
             total.append(corda[:])
             corda.clear()
+
+
+with open("usu.txt","a") as esc:
+        while sn == "s":
+            if err == 1:
+                nome = input("nome: ")
+                senha = input("senha: ")
+                corda.append(nome)
+                corda.append(senha)
+
+            
+            for i in total: 
+                    if nome == total[con][0]:
+                        err == 1
+                        
+                        con == 0
+                        print(f"{nome}  ==  {total[con][0]}")
+                        print("Nome ja utilizado")
+                        break
+                    else:
+                        print(f"{nome}  !=  {total[con][0]}")
+                        err = 0
+                    con+=1
+
+
+
+                    
+            else:
+                total.append(corda[:])
+                corda.clear()
+                abr = esc.write(f"\n{nome}\n\n{senha}\n")
+                sn = str(input("continuar: ")).lower()
+                err = 1
+                print("login Feito")
+
+
 
 
             
