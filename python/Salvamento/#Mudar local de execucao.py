eeb0ce1ae22618b7a8b522b7d8ee54de1e43e9fb
LuanFazolim <131609670+LuanFazolim@users.noreply.github.com>
2024-05-20 23:21:25 -0300

    Add files via upload

---
+++ python/Salvamento/#Mudar local de execucao.py
@@ -0,0 +1,9 @@
+#Mudar local de execucao
+
+import os
+
+# Diretório que você deseja definir como o novo diretório de trabalho
+novo_diretorio = '/caminho/para/novo/diretorio'
+
+# Alterar o diretório de trabalho
+os.chdir(novo_diretorio)
